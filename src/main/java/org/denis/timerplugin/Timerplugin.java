9c3b15265c48734c139a4dee44976d03029fceee
Denis Litwin <denislitwin@googlemail.com>
2026-02-20 23:34:50 +0100

    Initial commit

---
+++ src/main/java/org/denis/timerplugin/Timerplugin.java
@@ -0,0 +1,88 @@
+package org.denis.timerplugin;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.jetbrains.annotations.NotNull;
+
+public final class Timerplugin extends JavaPlugin implements CommandExecutor {
+
+    private boolean running = false;
+    private int timeElapsed = 0;
+
+    @Override
+    public void onEnable() {
+        if (getCommand("starttimer") != null) getCommand("starttimer").setExecutor(this);
+        if (getCommand("stoptimer") != null) getCommand("stoptimer").setExecutor(this);
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (label.equalsIgnoreCase("starttimer")) {
+            if (running) {
+                sender.sendMessage(Component.text("Timer already running!", NamedTextColor.RED));
+                return true;
+            }
+            running = true;
+            timeElapsed = 0;
+            startTimer();
+            sender.sendMessage(Component.text("Timer started!", NamedTextColor.GREEN));
+            return true;
+        }
+        if (label.equalsIgnoreCase("stoptimer")) {
+            if (!running) {
+                sender.sendMessage(Component.text("Timer already running!", NamedTextColor.RED));
+                return true;
+            }
+
+            running = false; // Beendet den Timer
+            sender.sendMessage(Component.text("Timer stopped " + formatTime(timeElapsed), NamedTextColor.YELLOW));
+            return true;
+        }
+
+        return false;
+    }
+
+    private void startTimer() {
+        new BukkitRunnable() {
+
+            @Override
+            public void run() {
+                if (!running) {
+                    this.cancel();
+                    return;
+                }
+                Component message = Component.text(formatTime(timeElapsed), NamedTextColor.DARK_PURPLE, TextDecoration.BOLD);
+
+                for (Player player : Bukkit.getOnlinePlayers()) {
+                    player.sendActionBar(message);
+                }
+
+                timeElapsed++;
+            }
+        }.runTaskTimer(this, 0L, 20L);
+    }
+
+    private String formatTime(int totalseconds) {
+
+        int days = totalseconds / 86400;
+        int hours = (totalseconds % 86400) / 3600;
+        int minutes = (totalseconds % 3600) / 60;
+        int seconds = totalseconds % 60;
+
+        if (days > 0) {
+            return String.format("%ddd %02dh %02dm %02ds", days, hours, minutes, seconds);
+        } else {
+            return String.format("%02dh %02dm %02ds", hours, minutes, seconds);
+        }
+
+    }
+}
+
